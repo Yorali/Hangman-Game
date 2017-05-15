3a458e99da4007f33188bce9098f5ae5ddcc79aa
Ajibola Ayeni <hideayeni@gmail.com>
2017-05-14 19:43:46 -0500

    ww

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+//Create a list of words
+
+var words= ['pain','adore','undying','disarray','derangment','loss'];
+
+// Create an alphabet to draw from when guessing (English)
+var alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+guess function(key) {
+	var key= //key press saved as a string
+	for (var i=0; i<words.length; i++) {
+		for (var j=0; j<words[j].length; j++) {
+			if (~~ === key)
+		}
+	}
+}
+
+
+
+for (var i=0; i<words.length; i++) {
+	for (var j=0; j<words[i].length; j++) {
+		var place=words[i].charAt(j);
+		console.log(place);
+	}
+}

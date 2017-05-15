a5984250250d9e79dcdc483282383b2123036b0a
Ajibola Ayeni <hideayeni@gmail.com>
2017-05-15 00:27:31 -0500

    www

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,20 +5,31 @@ var words= ['pain','adore','undying','disarray','derangment','loss'];
 // Create an alphabet to draw from when guessing (English)
 var alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-guess function(key) {
+// guess function(key) {
+// 	var key= //key press saved as a string
+// 	for (var i=0; i<words.length; i++) {
+// 		for (var j=0; j<words[i].length; j++) {
+// 			var place=words[i].charAt(j);
+// 			console.log(place);
+// 		}
+// 	}
+// }
+
+
+document.onkeypress = function (e) {
+    e = e || window.event;
+    // use e.keyCode
+    console.log(e);
+
+    guess function(key) {
 	var key= //key press saved as a string
 	for (var i=0; i<words.length; i++) {
-		for (var j=0; j<words[j].length; j++) {
-			if (~~ === key)
+		for (var j=0; j<words[i].length; j++) {
+			var place=words[i].charAt(j);
+			console.log(place);
 		}
 	}
 }
 
 
-
-for (var i=0; i<words.length; i++) {
-	for (var j=0; j<words[i].length; j++) {
-		var place=words[i].charAt(j);
-		console.log(place);
-	}
-}
+};
